f2776eb8a88de42aa06baffb79d5767863dd71e8
David O Mahony <dave.omahony@betwaygroup.com>
2021-12-30 16:54:54 +0000

    Adding more basic projects

---
+++ test/CryptoPredictor.CryptoInfoAPI.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CryptoPredictor.CryptoInfoAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
